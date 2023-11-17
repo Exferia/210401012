dcd604d1bac8f2f9645255a26ef407b36e2c5f80
Exferia <msfkose@gmail.com>
2023-11-17 13:44:37 +0300

    1.2, testleri ile tamamlandı

--- 210401012/Labex1_Q1_210401012_Yusuf_Kose.R
+++ 210401012/Labex1_Q1_210401012_Yusuf_Kose.R
@@ -1,1 +1,3 @@
 download.file("https://raw.githubusercontent.com/scizmeli/Red/master/MapsThatChangedOurWorld_StoryMap_Data.csv", "MapsThatChangedOurWorld_StoryMap_Data.csv")
+'Q1.2'
+maps <- read.csv("MapsThatChangedOurWorld_StoryMap_Data.csv", header=TRUE, sep=";")
---
+++ 210401012/Labex1_Q1_tests_210401012_Yusuf_Kose.R
@@ -0,0 +1,5 @@
+'Q2.1'
+rm(list = ls())
+if(file.exists("MapsThatChangedOurWorld_StoryMap_Data.csv")){
+  file.remove("MapsThatChangedOurWorld_StoryMap_Data.csv")
+}
